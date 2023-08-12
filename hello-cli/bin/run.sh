6a78db592ff9adc33abe009827269a79317d60f5
Jonathan Parker <joparker@penguinrandomhouse.com>
2023-08-12 17:02:19 -0400

    Added the run script

---
+++ hello-cli/bin/run.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# @(#)run.sh	1.0.0	08/12/2023
+#
+# Copyright (c) Jonathan M. Parker
+# 324 Lantana Drive
+# Owings Mills, MD 21117 U.S.A.
+# All Rights Reserved.
+#
+# @author       Jonathan Parker
+# @since        1.0.0
+# @version      1.0.0
+#
+# Usage:
+#       run.sh
+
+node --no-warnings .
