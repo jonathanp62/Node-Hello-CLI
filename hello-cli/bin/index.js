c39c3d1270fb18fb272b27a17fd5e25deb41ed7c
Jonathan Parker <joparker@penguinrandomhouse.com>
2023-08-12 11:09:43 -0400

    Added imports and reading from the package

--- hello-cli/bin/index.js
+++ hello-cli/bin/index.js
@@ -1,3 +1,21 @@
 #!/usr/bin/env node
 
-console.log("Hello!");
+import chalk from "chalk";
+import boxen from "boxen";
+import pkg from '../package.json' assert {type: 'json'};
+
+const hello = "Hello from version " + pkg.version + "!";
+const greeting = chalk.white.bold("Hello again from " + pkg.author + "!");
+
+const boxenOptions = {
+    padding: 1,
+    margin: 1,
+    borderStyle: "round",
+    borderColor: "green",
+    backgroundColor: "#555555"
+};
+
+const msgBox = boxen(greeting, boxenOptions);
+
+console.log(hello);
+console.log(msgBox);
